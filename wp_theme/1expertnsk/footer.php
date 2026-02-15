f6c8b5fe2cff43dccea052847724f362804b3417
Podrezov Sergey <podrezov@lanit.ru>
2026-02-16 00:20:48 +0500

    last changes

--- wp_theme/1expertnsk/footer.php
+++ wp_theme/1expertnsk/footer.php
@@ -14,8 +14,7 @@
         <?php dynamic_sidebar( 'footer-1' ); ?>
       <?php else : ?>
         <span class="title_contacts">
-          ПЕРВОЕ<br>
-          ЭКСПЕРТНОЕ БЮРО
+          ПЕРВОЕ ЭКСПЕРТНОЕ БЮРО
         </span><br>
         <span id="title_contacts_condenced">
           независимая судебная экспертиза<br>
@@ -50,6 +49,10 @@
           ?>
         </span>
       <?php endif; ?>
+      <br><br><br>
+        <a class="link_choice_footer" href="<?php echo esc_url( get_theme_mod( 'expertnsk_telegram', '/service' ) ); ?>" >
+    ВЫБРАТЬ ТИП ЭКСПЕРТИЗЫ
+  </a>
     </div>
 
     <div class="footer-column footer-contacts">
@@ -83,7 +86,7 @@
             <img class="icon" id="icon_letter_white" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/icons/icon_letter_white.png" alt="Email">
             <?php if ( get_theme_mod( 'expertnsk_contact_email' ) ) : ?>
               <a class="contacts_footer_link" href="<?php echo esc_url( expertnsk_format_email_link( get_theme_mod( 'expertnsk_contact_email' ) ) ); ?>">
-                email: <?php echo esc_html( get_theme_mod( 'expertnsk_contact_email' ) ); ?>
+                <?php echo esc_html( get_theme_mod( 'expertnsk_contact_email' ) ); ?>
               </a>
             <?php endif; ?>
           </div>
@@ -95,9 +98,7 @@
   <?php
   $reviews_link = get_theme_mod( 'expertnsk_reviews', 'https://novosibirsk.flamp.ru/firm/pervoe_ehkspertnoe_byuro_uchrezhdenie_nezavisimojj_ehkspertizy-70000001042868398' );
   ?>
-  <a class="link_choice_footer" href="<?php echo esc_url( get_theme_mod( 'expertnsk_telegram', '/service' ) ); ?>" >
-    ВЫБРАТЬ ТИП ЭКСПЕРТИЗЫ
-  </a>
+
 </footer>
 
 <?php wp_footer(); ?>
